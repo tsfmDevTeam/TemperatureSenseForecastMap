e3c2f0d9c1a4ef360e3c5b3b828120af01c24d2b
kimoto1212 <106810837+kimoto22@users.noreply.github.com>
2022-07-16 22:39:29 +0900

    Update wgbt.py
    現在からその日の終わりまでの１時間ごとの暑さ指数の予測値の入った配列を返すようにした

--- src/wgbt.py
+++ src/wgbt.py
@@ -3,67 +3,45 @@ from urllib import request
 
 
 def location2wgbt(ido: float, keido: float) -> float:
-    """WGBT温度の計算
-
-    緯度・経度を用いて，Open-Meteo API(open-meteo.com)から，
-        - 温度
-        - 湿度
-        - 直達日射量
-        - 散乱日射量
-        - 風速
-
-        を取得します
-        これらの値からWGBT温度を計算します
-        (https://blog.obniz.com/news/obniz-wbgt-service.html)
-
-    Args:
-        - ido (float): 緯度
-        - keido (float): 経度
-
-    Returns:
-        - wgbt (float): WGBT温度
-    """
+    wgbts_list = []
     url = (
         f"https://api.open-meteo.com/v1/forecast?latitude={ido}&longitude={keido}"
         "&hourly=temperature_2m,relativehumidity_2m,direct_radiation,diffuse_radiation,"
         "windspeed_10m&current_weather=true&timezone=Asia%2FTokyo"
     )
 
+
     with request.urlopen(url) as r:
         body = json.loads(r.read())
 
         tdate = body["current_weather"]["time"]
-        index = body["hourly"]["time"].index(tdate)
+        index_now_time = body["hourly"]["time"].index(tdate)
 
-        temperature = body["hourly"]["temperature_2m"][index]
-        humidity = body["hourly"]["relativehumidity_2m"][index]
-        direct_radiation = body["hourly"]["direct_radiation"][index] / 1000
-        diffuse_radiation = body["hourly"]["diffuse_radiation"][index] / 1000
-        windspeed_10m = abs(body["hourly"]["windspeed_10m"][index] * 1000 / 3600)
+        for index in range(index_now_time, index_now_time + (len(body["hourly"]["time"])-index_now_time)):
 
-    wgbt = (
-        0.735 * temperature
-        + 0.0374 * humidity
-        + 0.00292 * temperature * humidity
-        + 7.619 * (direct_radiation + diffuse_radiation)
-        - 4.557 * (direct_radiation + diffuse_radiation) ** 2
-        - 0.0572 * windspeed_10m
-        - 4.064
-    )
+            temperature = body["hourly"]["temperature_2m"][index]
+            humidity = body["hourly"]["relativehumidity_2m"][index]
+            direct_radiation = body["hourly"]["direct_radiation"][index] / 1000
+            diffuse_radiation = body["hourly"]["diffuse_radiation"][index] / 1000
+            windspeed_10m = abs(body["hourly"]["windspeed_10m"][index] * 1000 / 3600)
 
-    return wgbt
+            wgbt = (
+                    0.735 * temperature
+                    + 0.0374 * humidity
+                    + 0.00292 * temperature * humidity
+                    + 7.619 * (direct_radiation + diffuse_radiation)
+                    - 4.557 * (direct_radiation + diffuse_radiation) ** 2
+                    - 0.0572 * windspeed_10m
+                    - 4.064
+            )
+            wgbts_list.append(round(wgbt, 3))
+
+    # 現在からその日の終わりまでの１時間ごとの暑さ指数の予測値の入った配列
+    return wgbts_list
 
 
 def wgbt_indicator(WBGT: float) -> str:
-    """WGBT温度による熱中症リスクの診断
-
-    Args:
-        WBGT (float): WGBT温度
-
-    Returns:
-        message (str): 危険度合のメッセージ
-    """
-    status: list[str] = ["運動は原則中止", "厳重警戒（激しい運動は中止）", "警戒（積極的に休憩", "注意（積極的に水分補給）", "ほぼ安全（適宜水分補給）"]
+    status: list[str] = ["運動は原則中止", "厳重警戒（激しい運動は中止）", "警戒（積極的に休憩)", "注意（積極的に水分補給）", "ほぼ安全（適宜水分補給）"]
 
     if WBGT > 35:
         return status[0]
