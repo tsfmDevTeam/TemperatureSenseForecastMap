f07506c0d14ee0867808c96ca4bb01d7cc34dd6a
motty.mio2 <motty.mio2@gmail.com>
2022-07-16 14:29:56 +0900

    django init

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('Map/', views.MapView.as_view(), name='Map'),
+    path('Mapdetail/', views.MapView.basyodetail, name='Mapdetail'),
+]
