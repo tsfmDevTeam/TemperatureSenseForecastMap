02b3b53d1bdfba27ab444aafe9eb3ce35a2dfe57
motty.mio2 <motty.mio2@gmail.com>
2022-07-16 14:54:04 +0900

    fix wgbt

--- src/wgbt.py
+++ src/wgbt.py
@@ -17,16 +17,16 @@ def location2wgbt(ido: float, keido: float) -> float:
 
         temperature = body["hourly"]["temperature_2m"][index]
         humidity = body["hourly"]["relativehumidity_2m"][index]
-        direct_radiation = body["hourly"]["direct_radiation"][index]
-        diffuse_radiation = body["hourly"]["diffuse_radiation"][index]
-        windspeed_10m = body["hourly"]["windspeed_10m"][index]
+        direct_radiation = body["hourly"]["direct_radiation"][index] / 1000
+        diffuse_radiation = body["hourly"]["diffuse_radiation"][index] / 1000
+        windspeed_10m = abs(body["hourly"]["windspeed_10m"][index] * 1000 / 3600)
 
     wgbt = (
         0.735 * temperature
         + 0.0374 * humidity
         + 0.00292 * temperature * humidity
-        + 7.619 * (direct_radiation * diffuse_radiation)
-        - 4.557 * (direct_radiation * diffuse_radiation) ** 2
+        + 7.619 * (direct_radiation + diffuse_radiation)
+        - 4.557 * (direct_radiation + diffuse_radiation) ** 2
         - 0.0572 * windspeed_10m
         - 4.064
     )
