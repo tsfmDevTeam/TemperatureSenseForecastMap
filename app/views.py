f76913cb2ba83192f3a53d3713a7b7e1c230edab
kimoto1212 <106810837+kimoto22@users.noreply.github.com>
2022-07-16 22:38:35 +0900

    Update views.py
    暑さ指数とステータスのアップグレード

--- app/views.py
+++ app/views.py
@@ -15,23 +15,45 @@ class MapView(TemplateView):
         lat: float = request.POST.get("lat")
         lon: float = request.POST.get("lng")
 
-        wgbt_celsius = wgbt.location2wgbt(ido=lat, keido=lon)
-        wgbt_status = wgbt.wgbt_indicator(WBGT=wgbt_celsius)
+
+
+        wgbt_list = wgbt.location2wgbt(ido=lat, keido=lon)
+
+        wgbt_now = wgbt_list[0]
+        wgbt_status_now = wgbt.wgbt_indicator(WBGT=wgbt_now)
+
+        wgbt_max = max(wgbt_list)
+        wgbt_status_max = wgbt.wgbt_indicator(WBGT=wgbt_max)
+
+        wgbt_and_status: list[dict[str, str]] = []
+        for WGBT in wgbt_list:
+            status = wgbt.wgbt_indicator(WBGT=WGBT)
+            wgbt_and_status.append(
+                {
+                    "WGBT": WGBT,
+                    "status": status,
+                }
+            )
+
 
         # 周辺地域の取得
         tikaku = geo_apis.find_near(ido=lat, keido=lon)
-        print(tikaku)
-        if tikaku:
-            return render(
-                request,
-                "app/detail.html",
-                {
-                    "lat": lat,
-                    "lon": lon,
-                    "wgbt": wgbt_celsius,
-                    "wgbt_indicator": wgbt_status,
-                    "tikaku": tikaku,
-                },
-            )
-        else:
-            return render(request, "app/basyodetail_error.html")
+
+        return render(
+            request,
+            "app/detail.html",
+            {
+                "lat": lat,
+                "lon": lon,
+
+                "wgbt_now": wgbt_now,
+                "wgbt_max": wgbt_max,
+
+                "wgbt_status_now": wgbt_status_now,
+                "wgbt_status_max": wgbt_status_max,
+
+                "wgbt_and_status": wgbt_and_status,
+
+                "tikaku": tikaku,
+            },
+        )
