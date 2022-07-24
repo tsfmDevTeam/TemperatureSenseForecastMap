9cb29204503ed77878fc92f23a30c1f625ce7c8d
kimoto1212 <106810837+kimoto22@users.noreply.github.com>
2022-07-25 00:36:29 +0900

    Revert "24時間の暑さ指数をグラフにしました。"
    This reverts commit 133861fded3b82796af2bf590272d85b03e2d027.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
-
-class AmedasPoint(models.Model):
-    ido = models.FloatField(verbose_name="ido")
-    keido = models.FloatField(verbose_name="keido")
-    wgbt_24 = models.FloatField(verbose_name="wgbt_24")
+# Create your models here.
