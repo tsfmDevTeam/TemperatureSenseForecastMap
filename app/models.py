a1a01262d6f5afda456d9fc597ccd8f5224cd9e2
kimoto1212 <106810837+kimoto22@users.noreply.github.com>
2022-07-25 00:37:52 +0900

    モデルに緯度経度と２４時間分のwgbtを追加しました。 DBにAmedasPointのテーブルを追加しました。

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class AmedasPoint(models.Model):
+    ido = models.FloatField(verbose_name="ido")
+    keido = models.FloatField(verbose_name="keido")
+    wgbt_24 = models.FloatField(verbose_name="wgbt_24")
